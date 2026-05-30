4f73fe94068655f63ac835f5731b4b749c236907
田梓轩 <3824677436@qq.com>
2026-05-30 16:34:49 +0800

    feat(运营): 实现运营人员分页查询接口

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,4 +19,5 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
