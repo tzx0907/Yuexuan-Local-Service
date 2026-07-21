c993dde07f238d097dc50299c440bde34c1650d1
田梓轩 <3824677436@qq.com>
2026-07-21 11:15:35 +0800

    feat(订单): 增加订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -4,6 +4,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
@@ -19,4 +22,11 @@ public interface OrderMapper {
      * @param orders
      */
     void update(Orders orders);
+    /**
+     * 根据状态和时间取消订单
+     * @param pendingPayment
+     * @param time
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{time}")
+    List<Orders> cancelOrderByStatusAndTime(Integer pendingPayment, LocalDateTime time);
 }
---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,40 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Scheduled(cron="0 * * * * ?")
+    public void orderCancelTask() {
+        log.info("订单取消任务开始执行");
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> list=orderMapper.cancelOrderByStatusAndTime(Orders.PENDING_PAYMENT,time);
+        for(Orders orders:list){
+            orders.setStatus(Orders.CANCELLED);
+            orders.setCancelTime(LocalDateTime.now());
+            orders.setCancelReason("订单超时未支付，系统自动取消");
+            orderMapper.update(orders);
+        }
+    }
+    @Scheduled(cron="0 0 1 * * ?")
+    public void deliveryCancleTask() {
+        log.info("订单取消任务开始执行");
+        LocalDateTime time = LocalDateTime.now().plusHours(-1);
+        List<Orders> list=orderMapper.cancelOrderByStatusAndTime(Orders.DELIVERY_IN_PROGRESS,time);
+        for(Orders orders:list){
+            orders.setStatus(Orders.COMPLETED);
+            orderMapper.update(orders);
+        }
+    }
+}
